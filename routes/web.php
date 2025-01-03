dfc6c4bc80640be040333a1dbc1dec68e6a4daa5
cybelx <cptorefiel@up.edu.ph>
2025-01-04 02:35:25 +0800

    Added new routes for About Us, Contact Us, Services, Collection, Resources

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about_us', function () {
+    return view('aboutus');
+});
+
+Route::get('/contact_us', function () {
+    return view('contactus');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/collection', function () {
+    return view('collection');
+});
+
+Route::get('/resources', function () {
+    return view('resources');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
