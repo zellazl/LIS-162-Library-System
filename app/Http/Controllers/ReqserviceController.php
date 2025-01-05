be6e95717352b220bc94e4ae463d4fb7608ba296
Zoie <zmirasusta@up.edu.ph>
2025-01-05 10:07:39 +0800

    Added basic index in reqservice controller

---
+++ app/Http/Controllers/ReqserviceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reqservice;
+use Illuminate\Http\Request;
+
+class ReqserviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $reqservices = Reqservice::get()
+        return view ('reqservice.index', compact ('reqservices'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reqservice $reqservice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Reqservice $reqservice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Reqservice $reqservice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Reqservice $reqservice)
+    {
+        //
+    }
+}
