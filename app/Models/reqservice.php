a707a1a210ca718dd4045edb432f4c7d8ee9302c
Zoie <zmirasusta@up.edu.ph>
2025-01-05 11:35:21 +0800

    Edited model to reflect service date

--- app/Models/reqservice.php
+++ app/Models/reqservice.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class reqservice extends Model
 {
-    protected $fillable = ['id', 'created_at', 'updated_at', 'deleted_at', 'time_slot', 'service_name'];
+    protected $fillable = ['id', 'created_at', 'updated_at', 'deleted_at', 'service_date', 'time_slot', 'service_name'];
 }
