6740ae57496fc9ff19dbee7111ddb720e34a2d9f
Zoie <zmirasusta@up.edu.ph>
2025-01-05 10:20:09 +0800

    Created view reqservice with placeholder html

---
+++ resources/views/reqservice/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.219/dist/tailwind.min.css">
+
+<body class ="bg-yellow-200">
+    <title>Requests</title>
+    <header class="bg-yellow-600 w-screen py-5 flex pl-5 sticky top-0 z-10">
+        <div class="text-left">
+            <h1 class="text-4x1 font-black italic text-white">Services</h1>
+        </div>
+     </header>
