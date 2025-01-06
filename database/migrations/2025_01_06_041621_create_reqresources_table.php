db89e2ddf50729c1638daac4b51a793fe543a0dd
ccarlando <59926043+ccarlando@users.noreply.github.com>
2025-01-06 21:50:37 +0800

    Update 2025_01_06_041621_create_reqresources_table.php
    Added
    $table foreign
    $table unsignedBigInteger
    $table string (transaction stat)

--- database/migrations/2025_01_06_041621_create_reqresources_table.php
+++ database/migrations/2025_01_06_041621_create_reqresources_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('reqresources', function (Blueprint $table) {
             $table->id();
+            $table->foreign('user_id')->references('id')->on('users'); 
+            $table->unsignedBigInteger('user_id')->unsigned(); 
             $table->timestamps();
             $table->softDeletes();
             $table->string('user_fullname');
@@ -20,6 +22,7 @@ return new class extends Migration
             $table->string('resource_title');
             $table->string('resource_author');
             $table->string('resource_accession_number');
+            $table->string('transaction_status');
         });
     }
 
