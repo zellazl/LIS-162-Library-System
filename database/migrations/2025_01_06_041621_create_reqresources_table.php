af861857f32e0a1bf36e81b93c619f07c002ab10
Zoie <zmirasusta@up.edu.ph>
2025-01-06 12:42:02 +0800

    Created reqresources table (again, updated)

---
+++ database/migrations/2025_01_06_041621_create_reqresources_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reqresources', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('user_fullname');
+            $table->string('user_email');
+            $table->date('claim_date');
+            $table->string('resource_title');
+            $table->string('resource_author');
+            $table->string('resource_accession number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reqresources');
+    }
+};
