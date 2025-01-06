3d22ca5c81324b73dcbbea577a63f04a2198228d
Rey <jtreyes13@up.edu.ph>
2025-01-06 14:21:08 +0800

    updated reservation.show

--- resources/views/reqservice/show.blade.php
+++ resources/views/reqservice/show.blade.php
@@ -4,11 +4,11 @@
 <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css">
 
 <body class ="bg-yellow-200">
-    <title>Requests</title>
+    <title>Reservations</title>
     <header class="bg-yellow-600 w-screen py-5 flex pl-5 sticky top-0 z-10">
 
         <div class="text-left">
-            <h1 class="text-4x1 font-black italic text-white">Services</h1>
+            <h1 class="text-4x1 font-black italic text-white">Reservation</h1>
         </div>
      </header>
 
