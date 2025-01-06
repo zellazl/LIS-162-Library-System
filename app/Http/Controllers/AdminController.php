0601fac57d53ee310b74dbf6d8da56011b30a76c
ccarlando <59926043+ccarlando@users.noreply.github.com>
2025-01-06 20:53:58 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reqservice;
+use App\Models\Reqresource;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -11,10 +12,18 @@ class AdminController extends Controller
     {
         // Fetch all reqservices and reqresources from the database
         $reqservices = Reqservice::all();
-        //$reqResources = ReqResource::all();
+        $reqresources = Reqresource::all();
 
         // Pass the data to the admin view
-        //return view('admin.dashboard', compact('reqServices', 'reqResources'));
-        return view('admin.dashboard', compact('reqservices'));
+        return view('admin.dashboard', compact('reqservices', 'reqresources'));
+    }
+    
+     public function destroy(string $id)
+    {
+        $delRecord = Reqservice::findOrFail($id);
+        $delRecord = Reqresource::findOrFail($id);
+        $delRecord->delete();
+        
+        return redirect()->route('admins.index');
     }
 }
